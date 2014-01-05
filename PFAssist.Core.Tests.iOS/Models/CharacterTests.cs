00c121fce00b36a119be7adc186485842fada0fc
Steve McGuire <steveorsomethin@gmail.com>
2014-01-04 16:11:55 -0800

    Filled out basic binding in Character

--- PFAssist.Core.Tests.iOS/Models/CharacterTests.cs
+++ PFAssist.Core.Tests.iOS/Models/CharacterTests.cs
@@ -6,6 +6,50 @@ namespace PFAssist.Core.Tests.iOS
 	[TestFixture]
 	public class CharacterTests
 	{
+		[Test]
+		public void DexChangesAffectArmorClass ()
+		{
+			var character = new Character ();
+			var acDex = character.ArmorClass.Dexterity;
 
+			character.PrimaryStats.Dexterity.Modifier.OverrideWith(15);
+
+			Assert.AreEqual (acDex.Value, 15);
+		}
+
+		[Test]
+		public void DexChangesAffectInitiative ()
+		{
+			var character = new Character ();
+			var initDex = character.Initiative.Dexterity;
+
+			character.PrimaryStats.Dexterity.Modifier.OverrideWith(15);
+
+			Assert.AreEqual (initDex.Value, 15);
+		}
+
+		[Test]
+		public void WisdomAffectsMonkArmorClass ()
+		{
+			var character = new Character ();
+			var acMisc = character.ArmorClass.Miscellaneous;
+
+			character.Class1.Value = CharacterClasses.Monk;
+
+			character.PrimaryStats.Wisdom.Modifier.OverrideWith(15);
+
+			Assert.AreEqual (acMisc.Value, 15);
+		}
+
+		[Test]
+		public void WisdomDoesntAffectOtherArmorClass ()
+		{
+			var character = new Character ();
+			var acMisc = character.ArmorClass.Miscellaneous;
+
+			character.PrimaryStats.Wisdom.Modifier.OverrideWith(15);
+
+			Assert.AreNotEqual (acMisc.Value, 15);
+		}
 	}
 }
--- PFAssist.Core.iOS/Models/Character.cs
+++ PFAssist.Core.iOS/Models/Character.cs
@@ -1,41 +1,67 @@
 using System;
+using System.Reactive.Linq;
+using System.Collections.Generic;
 
-namespace PFAssist.Core.iOS
+namespace PFAssist.Core
 {
-	public enum Size
+	public enum Sizes
 	{
-		Small = 0,
-		Medium = 1,
-		Large = 2
+		Colossal = -8,
+		Gargantuan = -4,
+		Huge = -2,
+		Large = -1,
+		Medium = 0,
+		Small = 1,
+		Tiny = 2,
+		Diminutive = 4,
+		Fine = 8
 	}
 
-	public enum CharacterClass
+	public enum CharacterClasses
 	{
-		// TODO: Fill this out
-		Fighter
+		None,
+		Alchemist,
+		Antipaladin,
+		Barbarian,
+		Bard,
+		Cavalier,
+		Cleric,
+		Druid,
+		Fighter,
+		Gunslinger,
+		Inquisitor,
+		Magus,
+		Monk,
+		Oracle,
+		Paladin,
+		Ranger,
+		Rogue,
+		Sorcerer,
+		Summoner,
+		Witch,
+		Wizard
 	}
 
-	public enum Alignment
+	public enum Alignments
 	{
 		// TODO: Fill this out
 		Neutral
 	}
 
-	public enum Race
+	public enum Races
 	{
 		// TODO: Fill this out
 		Human
 	}
 
-	public enum Deity
+	public enum Deities
 	{
 		// TODO: Fill this out
 		Dickus
 	}
 
-	public enum Gender
+	public enum Genders
 	{
-		// TODO: Fill this out
 		Male,
 		Female,
 		Other
@@ -45,11 +71,11 @@ namespace PFAssist.Core.iOS
 	{
 		// Fluff fields
 		public readonly ReactiveValue<String> Name = new ReactiveValue<String> ();
-		public readonly ReactiveValue<Alignment> Alignment = new ReactiveValue<Alignment> ();
-		public readonly ReactiveValue<Race> Race = new ReactiveValue<Race> ();
-		public readonly ReactiveValue<Deity> Deity = new ReactiveValue<Deity> ();
+		public readonly ReactiveValue<Alignments> Alignment = new ReactiveValue<Alignments> ();
+		public readonly ReactiveValue<Races> Race = new ReactiveValue<Races> ();
+		public readonly ReactiveValue<Deities> Deity = new ReactiveValue<Deities> ();
 		public readonly ReactiveValue<String> Player = new ReactiveValue<String> ();
-		public readonly ReactiveValue<Gender> Gender = new ReactiveValue<Gender> ();
+		public readonly ReactiveValue<Genders> Gender = new ReactiveValue<Genders> ();
 		public readonly ReactiveValue<int> Age = new ReactiveValue<int> ();
 		public readonly ReactiveValue<int> Height = new ReactiveValue<int> ();
 		public readonly ReactiveValue<int> Weight = new ReactiveValue<int> ();
@@ -57,18 +83,36 @@ namespace PFAssist.Core.iOS
 		public readonly ReactiveValue<String> Eyes = new ReactiveValue<String> ();
 		public readonly ReactiveValue<String> Homeland = new ReactiveValue<String> ();
 		// Fields that affect downstream values
-		public readonly ReactiveValue<Size> Size = new ReactiveValue<Size> ();
-		public readonly ReactiveValue<CharacterClass> Class1 = new ReactiveValue<CharacterClass> ();
+		public readonly ReactiveValue<Sizes> Size = new ReactiveValue<Sizes> ();
+		public readonly ReactiveValue<CharacterClasses> Class1 = new ReactiveValue<CharacterClasses> ();
 		public readonly ReactiveValue<int> Level1 = new ReactiveValue<int> ();
-		public readonly ReactiveValue<CharacterClass> Class2 = new ReactiveValue<CharacterClass> ();
+		public readonly ReactiveValue<CharacterClasses> Class2 = new ReactiveValue<CharacterClasses> ();
 		public readonly ReactiveValue<int> Level2 = new ReactiveValue<int> ();
-		public readonly ReactiveValue<CharacterClass> Class3 = new ReactiveValue<CharacterClass> ();
+		public readonly ReactiveValue<CharacterClasses> Class3 = new ReactiveValue<CharacterClasses> ();
 		public readonly ReactiveValue<int> Level3 = new ReactiveValue<int> ();
 		// Composed bits
-		public Stats PrimaryStats = new Stats();
+		public readonly Stats PrimaryStats = new Stats ();
+		public readonly ArmorClass ArmorClass = new ArmorClass ();
+		public readonly Initiative Initiative = new Initiative ();
 
 		public Character ()
 		{
+			Size.Select (s => (int) s).Subscribe (ArmorClass.Size);
+
+			PrimaryStats.Dexterity.Modifier.Subscribe (ArmorClass.Dexterity);
+			PrimaryStats.Dexterity.Modifier.Subscribe (Initiative.Dexterity);
+
+			Observable.CombineLatest (
+				PrimaryStats.Wisdom.Modifier,
+				Class1,
+				Class2,
+				Class3,
+				(w, c1, c2, c3) => {
+					var monk = CharacterClasses.Monk;
+
+					return c1 == monk || c2 == monk || c3 == monk ? w : 0;
+				})
+				.Subscribe(ArmorClass.Miscellaneous);
 		}
 	}
 }
