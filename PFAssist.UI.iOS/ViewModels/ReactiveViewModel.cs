1a2cd70495803ec99d7bec8f481a149a84c390a7
Kennie Davis <kenniejaydavis@gmail.com>
2014-01-28 08:14:59 -0700

    Added a base reactive view model

---
+++ PFAssist.UI.iOS/ViewModels/ReactiveViewModel.cs
@@ -0,0 +1,42 @@
+using System;
+using ReactiveUI;
+using PFAssist.Core;
+
+namespace PFAssist.UI.iOS
+{
+	public class ReactiveViewModel<T> : ReactiveObject
+	{
+		public string Description {
+			get;
+			protected set;
+		}
+
+		public IReactiveValue<T> Data {
+			get;
+			protected set;
+		}
+
+		public ReactiveViewModel (string description, IReactiveValue<T> dataSource)
+		{
+			this.Description = description;
+			dataSource.Subscribe (this.Data);
+		}
+	}
+
+	public class CalculatedValueViewModel : ReactiveViewModel<Int32>
+	{
+		public CalculatedValueViewModel(string description, IReactiveValue<Int32> dataSource)
+			: base (description, dataSource)
+		{
+		}
+	}
+
+	public class StringValueViewModel : ReactiveViewModel<String>
+	{
+		public StringValueViewModel(string description, IReactiveValue<String> dataSource)
+			: base (description, dataSource)
+		{
+		}
+	}
+}
+
